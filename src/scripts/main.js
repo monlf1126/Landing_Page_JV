2133fdc299acdec03869eb6af0e6bb6409695d4f
Kevhec <kevhec.dev@gmail.com>
2024-01-23 22:19:24 -0500

    [ADD] Position aware btn, social accordion, [REFACTOR] Css

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
 import { register } from "swiper/element/bundle"
 
 register()
+
+const getButtonProps = (button) => {
+  const buttonRect = button.getBoundingClientRect();
+
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  const yPos = buttonRect.top + scrollTop;
+  const xPos = buttonRect.left;
+  const width = button.offsetWidth;
+
+  return {
+    yPos,
+    xPos,
+    width,
+  };
+};
+
+const positionSpan = ({
+  span, mouseEvent, xPos, yPos,
+}) => {
+  const { clientX, pageY } = mouseEvent;
+
+  const relX = clientX - xPos;
+  const relY = pageY - yPos;
+
+  span.style.setProperty('--top', `${relY}px`);
+  span.style.setProperty('--left', `${relX}px`);
+};
+
+const applyButtonsVariables = () => {
+  const buttons = document.querySelectorAll('.button--positionAware');
+
+  buttons.forEach((button) => {
+    const { xPos, yPos, width } = getButtonProps(button);
+
+    button.style.setProperty('--button-width', `${width}px`);
+
+    const handleButtonEvent = (evt) => {
+      const span = button.querySelector('span');
+      if (!span) return;
+      positionSpan({
+        span, mouseEvent: evt, xPos, yPos,
+      });
+    };
+    button.removeEventListener('mouseenter', handleButtonEvent);
+    button.removeEventListener('mouseleave', handleButtonEvent);
+
+    button.addEventListener('mouseenter', handleButtonEvent);
+    button.addEventListener('mouseleave', handleButtonEvent);
+  });
+};
+
+applyButtonsVariables();
+
+window.addEventListener('resize', () => {
+  applyButtonsVariables();
+});
